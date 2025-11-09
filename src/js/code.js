64c56435d2b25d6232540044532d09455c47ad9f
LuisMedinaDev <luisdev002@gmail.com>
2025-11-09 15:35:18 -0400

    n

--- src/js/code.js
+++
@@ -1,8 +0,0 @@
-const nav = document.getElementById('navegacion');
-const hamburguesa = document.getElementById('hambuerguesa');
-
-hamburguesa.addEventListener('click', () => {
-  const activo = nav.classList.toggle('activo');
-  hamburguesa.classList.toggle('color', activo); 
-  nav.classList.toggle('clipa', activo);
-});
